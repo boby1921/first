497302a39f0883a24d822f2697e4ed5bb69a34d4
mak <bmakhlouf@hotmail.fr>
2015-09-07 16:42:08 +0200

    mkamkamak

--- src/main/java/fr/treeptik/lol/Lol.java
+++ src/main/java/fr/treeptik/lol/Lol.java
@@ -21,11 +21,7 @@ public class Lol {
 
 		System.out.println("Hello world!");
 		System.out.println(1+2);
-		System.out.println("Alex le coach !!");
-		System.out.println("maklouf le roi du ouf");
-		System.out.println("coucou coucou coucou");
-		System.out.println("c'est chaud");
-		System.out.println("makmakmakmakmak");
+
 		
 	}
 }
